7d04ec1ab986b15c30ee75f999010785ca6a9ced
ykhwong <ykhwong@daum.net>
2018-12-07 20:13:18 +0900

    [ADD] Support for 4x4 grid

--- backend.js
+++ backend.js
@@ -61,41 +61,67 @@ app.on('ready', function() {
 	});
 
 	ipc.on('remote', (event, data) => {
+		var grid_case = Boolean(data.match(new RegExp("^open_","")));
 		var screenElectron = electron.screen;
 		var mainScreen = screenElectron.getPrimaryDisplay();
 		var dimensions = mainScreen.size;
 		var r_width = 0;
 		var r_height = 0;
 
-		if (data == "open_2x2") {
-			r_width = dimensions.width / 2;
-			r_height = dimensions.height / 2;
+		if (grid_case) {
 			subwin = [];
-			subwin[0] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, 0));
-			subwin[1] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, 0));
-			subwin[2] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, r_height));
-			subwin[3] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, r_height));
-		} else if (data == "open_4x4") {
-			// 4x4 is not supported as of now
-		}
-		if (data.match(new RegExp("^open_",""))) {
-			for (var i = 0; i < subwin.length; i++) {
-				subwin[i].loadURL(frontend_dir + '/grid_window_webview.html');
-				subwin[i].focus();
-				subwin[i].on('close', function(e){
-					for (var i = 0; i < subwin.length; i++) {
-						if (!subwin[i].isDestroyed()) {
-							subwin[i].destroy();
+			if (data == "open_2x2") {
+				r_width = dimensions.width / 2;
+				r_height = dimensions.height / 2;
+				subwin[0] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, 0));
+				subwin[1] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, 0));
+				subwin[2] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, r_height));
+				subwin[3] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, r_height));
+			} else if (data == "open_4x4") {
+				r_width = dimensions.width / 4;
+				r_height = dimensions.height / 4;
+				
+				subwin[0] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, 0));
+				subwin[1] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, 0));
+				subwin[2] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 2, 0));
+				subwin[3] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 3, 0));
+				
+				subwin[4] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, r_height));
+				subwin[5] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, r_height));
+				subwin[6] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 2, r_height));
+				subwin[7] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 3, r_height));
+
+				subwin[8] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, r_height * 2));
+				subwin[9] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, r_height * 2));
+				subwin[10] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 2, r_height * 2));
+				subwin[11] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 3, r_height * 2));
+
+				subwin[12] = new BrowserWindow(createWMpoperty(r_width, r_height, 0, r_height * 3));
+				subwin[13] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width, r_height * 3));
+				subwin[14] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 2, r_height * 3));
+				subwin[15] = new BrowserWindow(createWMpoperty(r_width, r_height, r_width * 3, r_height * 3));
+			}
+
+			if (data.match(new RegExp("^open_",""))) {
+				for (var i = 0; i < subwin.length; i++) {
+					subwin[i].loadURL(frontend_dir + '/grid_window_webview.html');
+					subwin[i].focus();
+					subwin[i].on('close', function(e){
+						for (var i = 0; i < subwin.length; i++) {
+							if (!subwin[i].isDestroyed()) {
+								subwin[i].destroy();
+							}
 						}
-					}
-				});
-				subwin[i].on('focus', function(e){
-					for (var i = 0; i < subwin.length; i++) {
-						subwin[i].setAlwaysOnTop(true);
-					}
-				});
+					});
+					subwin[i].on('focus', function(e){
+						for (var i = 0; i < subwin.length; i++) {
+							subwin[i].setAlwaysOnTop(true);
+						}
+					});
+				}
 			}
 		}
+
 	});
 
 	function loadExtensions() {
